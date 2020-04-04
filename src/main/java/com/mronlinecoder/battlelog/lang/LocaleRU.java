d2b89d00fff10f22d6f107a9e348d38e3cb56c19
Kaeios <kaeios.contact@gmail.com>
2020-04-04 13:38:42 +0200

    Changed file encoding to UTF-8

--- src/main/java/com/mronlinecoder/battlelog/lang/LocaleRU.java
+++ src/main/java/com/mronlinecoder/battlelog/lang/LocaleRU.java
@@ -2,11 +2,11 @@ package com.mronlinecoder.battlelog.lang;
 
 public class LocaleRU extends BattleLocale {
 	public LocaleRU() {
-		add("battle_start", "Вы вошли в режим боя! Отключение приведет к потере вещей!");
-		add("battle_end", "Вы вышли из боя.");
-		add("battle_info_false", "Вы сейчас не в бою.");
-		add("battle_info_true", "Вы будете в бою еще % сек.");
-		add("battle_cmd_deny", "Команды отключены во время боя!");
-		add("battle_punish", "Вы были наказаны за побег с поля боя!");
+		add("battle_start", "Р’С‹ РІРѕС€Р»Рё РІ СЂРµР¶РёРј Р±РѕСЏ! РћС‚РєР»СЋС‡РµРЅРёРµ РїСЂРёРІРµРґРµС‚ Рє РїРѕС‚РµСЂРµ РІРµС‰РµР№!");
+		add("battle_end", "Р’С‹ РІС‹С€Р»Рё РёР· Р±РѕСЏ.");
+		add("battle_info_false", "Р’С‹ СЃРµР№С‡Р°СЃ РЅРµ РІ Р±РѕСЋ.");
+		add("battle_info_true", "Р’С‹ Р±СѓРґРµС‚Рµ РІ Р±РѕСЋ РµС‰Рµ % СЃРµРє.");
+		add("battle_cmd_deny", "РљРѕРјР°РЅРґС‹ РѕС‚РєР»СЋС‡РµРЅС‹ РІРѕ РІСЂРµРјСЏ Р±РѕСЏ!");
+		add("battle_punish", "Р’С‹ Р±С‹Р»Рё РЅР°РєР°Р·Р°РЅС‹ Р·Р° РїРѕР±РµРі СЃ РїРѕР»СЏ Р±РѕСЏ!");
 	}
 }
