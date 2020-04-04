3ba4091b9948bb4a692b26e3cfad3c22348b1630
Kaeios <kaeios.contact@gmail.com>
2020-04-04 13:36:58 +0200

    Added French language

---
+++ src/main/java/com/mronlinecoder/battlelog/lang/LocaleFR.java
@@ -0,0 +1,14 @@
+package com.mronlinecoder.battlelog.lang;
+
+public class LocaleFR extends BattleLocale {
+
+    public LocaleFR() {
+        add("battle_start", "Vous êtes en combat ! Ne déconnectez pas !");
+        add("battle_end", "Vous n'êtes plus en combat. Vous pouvez déconnecter.");
+        add("battle_info_false", "Vous n'êtes pas en combat.");
+        add("battle_info_true", "Vous êtes en combat pendant encore % secondes.");
+        add("battle_cmd_deny", "Cette commande est désactivée en combat !");
+        add("battle_punish", "Vous avez été puni pour avoir quitté le combat !");
+    }
+
+}
